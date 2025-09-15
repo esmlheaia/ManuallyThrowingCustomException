4c914c57322eab40d9b740e977bf205a18d7e008
esmlheaia <miya.soleneee@gmail.com>
2025-09-15 22:33:45 +0800

    add a try-catch method

--- ManuallyThrowingCustomException/Form1.cs
+++ ManuallyThrowingCustomException/Form1.cs
@@ -40,7 +40,15 @@ namespace ManuallyThrowingCustomException
         private void frmAddProduct_Load(object sender, EventArgs e)
         {
             //array list for list of product category
-            string[] ListOfProductCategory = new string[] { "Beverages", "Bread/Bakery", "Canned/ Jarred Goods", "Dairy", "Frozen Goods", "Meat", "Personal Care", "Other" };
+            string[] ListOfProductCategory = new string[] { 
+                "Beverages", 
+                "Bread/Bakery", 
+                "Canned/ Jarred Goods", 
+                "Dairy", 
+                "Frozen Goods", 
+                "Meat", 
+                "Personal Care", 
+                "Other" };
             
             foreach (stirng category in ListOfProductCategory)
             {
@@ -49,20 +57,47 @@ namespace ManuallyThrowingCustomException
         }
             public string Product_Name(string name)
         {
+            try
+            {
             if (!Regex.IsMatch(name, @"^[a-zA-Z]+$"))
-                //Exception here
+            {
+                throw new StringFormatException(name);
+            }
+        }
+            catch (StringFormatException ex)
+            {
+                MessageBox.Show("Invalid Product Name: " + ex.Message);
+            }
                 return name;
         }
         public int Quantity(string qty)
         {
-            if (!Regex.IsMatch(qty, @"^[0-9]"))
-                //Exception here
+            try
+            {
+                if (!Regex.IsMatch(qty, @"^[0-9]"))
+                {
+                    throw new StringFormatException(qty);
+                }
+            }
+            catch (NumberFormatException ex)
+            {
+                MessageBox.Show("Invalid Quantity: " + ex.Message);
+            }
                 return Convert.ToInt32(qty);
         }
         public double SellingPrice(string price)
         {
-            if (!Regex.IsMatch(price.ToString(), @"^(\d*\.)?\d+$"))
-                //Exception here
+            try
+            {
+                if (!Regex.IsMatch(price.ToString(), @"^(\d*\.)?\d+$"))
+                {
+                    throw new CurrencyFormatException(price);
+                }
+            }
+            catch (CurrencyFormatException ex)
+            {
+                MessageBox.Show("Invalid Selling Price: " + ex.Message);
+            }
                 return Convert.ToDouble(price);
         }
         private void btnAddProduct_Click(object sender, EventArgs e)
