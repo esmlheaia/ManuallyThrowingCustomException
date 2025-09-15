d66d1eab22fd85b95d453486734135dd75fc7c54
esmlheaia <miya.soleneee@gmail.com>
2025-09-15 21:41:56 +0800

    put a binding source

--- ManuallyThrowingCustomException/Form1.cs
+++ ManuallyThrowingCustomException/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,15 +17,39 @@ namespace ManuallyThrowingCustomException
         private string _ProductName, _Category, _MfgDate, _ExpDate, _Description;
         private int _Quantity;
         private double _SellPrice;
+        public BindingSource showProductList;
         public frmAddProduct()
         {
             InitializeComponent();
+            showProductList = new BindingSource();
         }
 
         private void frmAddProduct_Load(object sender, EventArgs e)
         {
             //array list for list of product category
-            string[] ListOfProductCategory  = { "Beverages", "Bread/Bakery", "Canned/ Jarred Goods", "Dairy", "Frozen Goods", "Meat", "Personal Care", "Other" };
+            string[] ListOfProductCategory = { "Beverages", "Bread/Bakery", "Canned/ Jarred Goods", "Dairy", "Frozen Goods", "Meat", "Personal Care", "Other" };
         }
-    }
+        foreach(string category in ListOfProductCategory)
+        {
+            cmbCategory.Items.Add(category);
+        }
+}
+public string Product_Name(string name)
+{
+    if (!Regex.IsMatch(name, @"^[a-zA-Z]+$"))
+        //Exception here
+        return name;
+}
+public int Quantity(string qty)
+{
+    if (!Regex.IsMatch(qty, @"^[0-9]"))
+        //Exception here
+        return Convert.ToInt32(qty);
+}
+public double SellingPrice(string price)
+{
+    if (!Regex.IsMatch(price.ToString(), @"^(\d*\.)?\d+$"))
+        //Exception here
+        return Convert.ToDouble(price);
+}
 }
